cc92b87e6c039a9c645092bcf0576904f34b0f5c
jerrard suell <seulljay@gmail.com>
2019-10-23 23:08:52 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,7 +21,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-
+[["Bruce Green", "Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."], ["Gerhard Schtitt", "Hal Incandenza", "Lyle", "Mario Incandenza","Michael Pemulis"]]
 end
 
 def matrix_lookup(matrix, row, column)
