9de72b657f0c374485338a40b4c6899162f5b1d8
jerrard suell <seulljay@gmail.com>
2019-10-23 23:03:47 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-
+[["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"], ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza","Michael Pemulis"]]
 end
 
 def array_literal_matrix
